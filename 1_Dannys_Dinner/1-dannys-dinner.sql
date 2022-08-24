99ce1788c6095c1b4ffde157056b603e7fa07624
Simon <sszpunar0303@gmail.com>
2022-08-24 19:12:37 +0200

    change questions format

--- 1_Dannys_Dinner/1-dannys-dinner.sql
+++ 1_Dannys_Dinner/1-dannys-dinner.sql
@@ -1,17 +1,5 @@
--- 1. What is the total amount each customer spent at the restaurant?
--- 2. How many days has each customer visited the restaurant?
--- 3. What was the first item from the menu purchased by each customer?
--- 4. What is the most purchased item on the menu and how many times was
--- it purchased by all customers?
--- 5. Which item was the most popular for each customer?
--- 6. Which item was purchased first by the customer after they became a member?
--- 7. Which item was purchased just before the customer became a member?
--- 8. What is the total items and amount spent for each member before they became a member?
--- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier how many points would each customer have?
--- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi -
--- how many points do customer A and B have at the end of January?
 
--- 1.
+-- 1. What is the total amount each customer spent at the restaurant?
 
 select sales.customer_id, sum(menu.price) as "Amount Spent"
 from dannys_diner.sales as sales join dannys_diner.menu as menu
@@ -19,14 +7,14 @@ on sales.product_id = menu.product_id
 group by customer_id
 order by customer_id;
 
--- 2.
+-- 2. How many days has each customer visited the restaurant?
 
 select customer_id, count(distinct order_date)
 from dannys_diner.sales
 group by customer_id
 order by customer_id;
 
--- 3.
+-- 3. What was the first item from the menu purchased by each customer?
 
 with ordered as
 	( select customer_id, order_date, product_name,
@@ -40,7 +28,7 @@ from ordered
 where rank = 1
 group by customer_id, product_name
 
--- 4.
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
 select menu.product_name, count(menu.product_name)
 from dannys_diner.sales as sales join dannys_diner.menu
@@ -48,7 +36,7 @@ on sales.product_id = menu.product_id
 group by product_name
 order by count desc;
 
--- 5
+-- 5 Which item was the most popular for each customer?
 
 with customer as (
 select customer_id, product_id,
@@ -62,7 +50,7 @@ from customer join dannys_diner.menu
 on customer.product_id = menu.product_id
 group by product_name;
 
--- 6
+-- 6 Which item was purchased first by the customer after they became a member?
 
 with min_date as
 (
@@ -79,3 +67,9 @@ from min_date join dannys_diner.menu
 on min_date.product_id = menu.product_id
 where dense_rank = 1
 order by order_date
+
+-- 7. Which item was purchased just before the customer became a member?
+-- 8. What is the total items and amount spent for each member before they became a member?
+-- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier how many points would each customer have?
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi -
+-- how many points do customer A and B have at the end of January?
